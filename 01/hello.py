89b1fd3006b6ffc5f0958360fb01ef719a48b959
Rob Johnson <Johnson.rob.alan@gmail.com>
2023-01-03 21:15:52 -0800

    add prompt for name

--- 01/hello.py
+++ 01/hello.py
@@ -1,5 +1,7 @@
 def hello() -> None:
     print("Hello, world!")
+    name = input("What is your name?\n")
+    print(f"Hello, {name}.")
 
 
 if __name__ == "__main__":
--- 01/test_hello.py
+++ 01/test_hello.py
@@ -1,7 +1,18 @@
+import pytest
 import hello
 
 
-def test_hello(capsys):
+@pytest.mark.parametrize("name", ("Alice", "Bob", "Carl"))
+def test_hello(capsys, monkeypatch, name):
+    def mock_input(prompt):
+        print(prompt, end="")
+        print(name)
+        return name
+
+    monkeypatch.setattr("builtins.input", mock_input)
     hello.hello()
     output = capsys.readouterr().out
-    assert output == "Hello, world!\n"
+    assert (
+        output
+        == f"Hello, world!\nWhat is your name?\n{name}\nHello, {name}.\n"
+    )
