9f3252c245ebc9c38d336c038b3c4d74f30a162b
Rob Johnson <Johnson.rob.alan@gmail.com>
2023-01-03 21:30:35 -0800

    parametrize test

--- 01/02/test_temperature.py
+++ 01/02/test_temperature.py
@@ -1,5 +1,7 @@
 import temperature
+import pytest
 
 
-def test_f_to_c():
-    assert temperature.convertToCelsius(32) == 0
+@pytest.mark.parametrize("f, c", [(32, 0)])
+def test_f_to_c(f, c):
+    assert temperature.convertToCelsius(f) == c
