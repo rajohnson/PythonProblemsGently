87f5bc772420a2abfd97d657f8641e4e39cbf8f7
Rob Johnson <Johnson.rob.alan@gmail.com>
2023-01-03 21:31:20 -0800

    test boiling point

--- 01/02/test_temperature.py
+++ 01/02/test_temperature.py
@@ -2,6 +2,6 @@ import temperature
 import pytest
 
 
-@pytest.mark.parametrize("f, c", [(32, 0)])
+@pytest.mark.parametrize("f, c", [(32, 0), (212, 100)])
 def test_f_to_c(f, c):
     assert temperature.convertToCelsius(f) == c
