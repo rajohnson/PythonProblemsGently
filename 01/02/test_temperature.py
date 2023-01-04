ba2ef08d64af4655df47dc1dbaec5bc1c1b19c69
Rob Johnson <Johnson.rob.alan@gmail.com>
2023-01-03 21:50:42 -0800

    round trip tests

--- 01/02/test_temperature.py
+++ 01/02/test_temperature.py
@@ -20,3 +20,21 @@ def test_f_to_c(f, c):
 )
 def test_c_to_f(c, f):
     assert temperature.convertToFahrenheit(c) == f
+
+
+@pytest.mark.parametrize(
+    "start", [-100, -50, 0, 25, 50, 75, 100, 1000, 10_000]
+)
+def test_convert_round_trip_c_to_f_to_c(start):
+    assert start == temperature.convertToCelsius(
+        temperature.convertToFahrenheit(start)
+    )
+
+
+@pytest.mark.parametrize(
+    "start", [-100, -50, 0, 25, 50, 75, 100, 1000, 10_000]
+)
+def test_convert_round_trip_f_to_c_to_f(start):
+    assert start == temperature.convertToFahrenheit(
+        temperature.convertToCelsius(start)
+    )
