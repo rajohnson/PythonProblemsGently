ad54aa71ac254d6789092dbf9e358aab34e6aeb2
Rob Johnson <johnson.rob.alan@gmail.com>
2023-02-19 11:32:54 -0800

    test for bools with is instead of ==

--- 03/test_odd_even.py
+++ 03/test_odd_even.py
@@ -4,19 +4,19 @@ import pytest
 
 @pytest.mark.parametrize("num", [-2, 0.5, 0, 4.2, 8, 100, 212, 1_000_000, 2.0])
 def test_odd_with_not_odd(num):
-    assert isOdd(num) == False
+    assert isOdd(num) is False
 
 
 @pytest.mark.parametrize("num", [-1, 5, 7, 107, 219, 1_000_001, 3.0])
 def test_odd_with_odd(num):
-    assert isOdd(num) == True
+    assert isOdd(num) is True
 
 
 @pytest.mark.parametrize("num", [-1, 0.5, 4.2, 5, 7, 107, 219, 1_000_001, 3.0])
 def test_even_with_not_even(num):
-    assert isEven(num) == False
+    assert isEven(num) is False
 
 
 @pytest.mark.parametrize("num", [-2, 0, 8, 100, 212, 1_000_000, 2.0])
 def test_even_with_even(num):
-    assert isEven(num) == True
+    assert isEven(num) is True
