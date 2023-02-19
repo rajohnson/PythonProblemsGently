30a4a26a340831086221cdf1297e85620ddeb084
Rob Johnson <johnson.rob.alan@gmail.com>
2023-02-19 12:12:29 -0800

    ex14 average

---
+++ 14/test_average.py
@@ -0,0 +1,25 @@
+import random
+
+import pytest
+from average import average
+
+
+@pytest.mark.parametrize(
+    "input_values, expected",
+    [
+        ([1, 2, 3], 2),
+        ([1, 2, 3, 1, 2, 3, 1, 2, 3], 2),
+        ([12, 20, 37], 23),
+        ([0, 0, 0, 0, 0, 0], 0),
+    ],
+)
+def test_average(input_values, expected):
+    assert average(input_values) == expected
+
+
+def test_order_independent():
+    random.seed(42)
+    test_data = [1, 2, 3, 1, 2, 3, 1, 2, 3]
+    for _ in range(100):
+        random.shuffle(test_data)
+        assert average(test_data) == 2
