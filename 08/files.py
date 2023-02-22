9732449d49da767650b5f9e9b309fb98780dd187
Rob Johnson <johnson.rob.alan@gmail.com>
2023-02-21 21:43:10 -0800

    Allow path as argument in addition to str paths

--- 08/files.py
+++ 08/files.py
@@ -1,14 +1,17 @@
-def writeToFile(filename: str, text: str) -> None:
+from pathlib import Path
+
+
+def writeToFile(filename: str | Path, text: str) -> None:
     with open(filename, "w") as file_out:
         file_out.write(text)
 
 
-def appendToFile(filename: str, text: str) -> None:
+def appendToFile(filename: str | Path, text: str) -> None:
     with open(filename, "a") as file_out:
         file_out.write(text)
 
 
-def readFromFile(filename: str) -> str:
+def readFromFile(filename: str | Path) -> str:
     with open(filename) as file_in:
         data = file_in.read()
     return data
